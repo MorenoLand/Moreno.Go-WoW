dd49667699fc3f2399f7b23f59f5888b7d27660e
Denveous <tward1189@proton.me>
2026-09-05 17:22:48 -0400

    Correct character geoset regression test

--- render/m2_test.go
+++ render/m2_test.go
@@ -737,15 +737,8 @@ func TestLiveHumanCreateGeosets(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	hasBareLowerLeg := false
-	for _, submesh := range skin.submeshes {
-		if submesh.submeshID == 701 {
-			hasBareLowerLeg = true
-			break
-		}
-	}
-	if hasBareLowerLeg && !active[701] {
-		t.Fatalf("Human create did not select bare lower-leg geoset 701: %v", active)
+	if !active[702] {
+		t.Fatalf("Human create did not select default ear geoset 702: %v", active)
 	}
 	facial100, facial200, facial300 := resolveCharacterFacialHair(loader, world.Character{Race: 1, Gender: 0})
 	t.Logf("Human default facial geosets=%d,%d,%d", facial100, facial200, facial300)
